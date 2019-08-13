dd1c73b8f98138306aecd1d11a9cf222a78c61d0
bloshchytska <naya.kirikia@gmail.com>
2019-08-13 19:01:48 +0200

    Updates

---
+++ precache-manifest.2868a270e7265a42c9313de7781392e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73337bf00c59ea82ba451db73e4779b7",
+    "url": "/saltmuffin/index.html"
+  },
+  {
+    "revision": "ca3c7fb209a36c0008d0",
+    "url": "/saltmuffin/static/css/main.b8c7615a.chunk.css"
+  },
+  {
+    "revision": "1c45192532aa977927f2",
+    "url": "/saltmuffin/static/js/2.d4bb6c7f.chunk.js"
+  },
+  {
+    "revision": "ca3c7fb209a36c0008d0",
+    "url": "/saltmuffin/static/js/main.dfe7d1f0.chunk.js"
+  },
+  {
+    "revision": "b39e97eaa2e95abde5bb",
+    "url": "/saltmuffin/static/js/runtime~main.a671b4ac.js"
+  }
+]);
