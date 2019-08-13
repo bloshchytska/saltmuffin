1a0603bd5cb54f001d988101a89a5303a1ea9587
bloshchytska <naya.kirikia@gmail.com>
2019-08-13 21:00:15 +0200

    updates

---
+++ saltmuffin-app/src/components/Welcome-Buttton/WelcomeButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './WelcomeButton.css';
+
+const welcomeButton = (props) => {
+    return (
+        <div className="body-actions__action">
+            <div className="body-actions__action__underline">
+                <img alt={props.buttonTitle}
+                     src={props.buttonImg}/>
+                <span>{props.buttonTitle}</span>
+            </div>
+
+        </div>
+    );
+};
+
+export default welcomeButton;
