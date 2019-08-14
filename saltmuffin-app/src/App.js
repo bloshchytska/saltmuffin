e61faa7b0d2fa8ec2af74fdb9456655bdb3eb892
bloshchytska <naya.kirikia@gmail.com>
2019-08-15 00:55:11 +0200

    updates

--- saltmuffin-app/src/App.js
+++ saltmuffin-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import './App.scss';
 import Welcome from './views/Welcome/Welcome';
 import Contact from './views/Contact/Contact';
@@ -8,10 +8,10 @@ class App extends Component {
   render() {
     return (
         <div>
-          <Switch>
-            <Route exact path="/"  component={ Welcome }/>
-            <Route path="/contact"  component={ Contact }/>
-          </Switch>
+            <HashRouter basename="/">
+                <Route exact path="/"  component={ Welcome }/>
+                <Route path="/contact"  component={ Contact }/>
+            </HashRouter>
         </div>
     );
   }
