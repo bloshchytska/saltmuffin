9f64a2c2cac3ffb5165f34393c3e7d069b5ef9a8
bloshchytska <naya.kirikia@gmail.com>
2019-08-17 23:04:22 +0200

    updates

--- saltmuffin-app/src/views/Salt/Salt.js
+++ saltmuffin-app/src/views/Salt/Salt.js
@@ -6,11 +6,17 @@ class Salt extends Component {
     render() {
         return(
             <div className="salt">
-                <div>
+                <div className="salt-sky">
                     <Sky/>
                 </div>
                 <BWLayout>
-                    <br/>
+                    <div className="iframe-wrapper">
+                        <iframe
+                            title="b1-sticker"
+                            src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fkirikia%2Fposts%2F10211190677602011&width=500"
+                            scrolling="no" frameBorder="0"
+                            allowtransparency="true" allow="encrypted-media"></iframe>
+                    </div>
                 </BWLayout>
             </div>
         )
