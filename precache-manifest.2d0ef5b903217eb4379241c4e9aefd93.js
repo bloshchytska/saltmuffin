cb6596ba253f53bbf1fa8a3832e43b2aef36fe2b
bloshchytska-anastasiia <a.bloshchytska@reply.de>
2019-08-20 23:27:33 +0200

    Updates

---
+++ precache-manifest.2d0ef5b903217eb4379241c4e9aefd93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe23a14b696bfd0e3701fb968e15ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1bb7fc69ecb528e9c46",
+    "url": "/static/css/main.f44d608e.chunk.css"
+  },
+  {
+    "revision": "ad21ba5d1a581603c645",
+    "url": "/static/js/2.10564e64.chunk.js"
+  },
+  {
+    "revision": "a1bb7fc69ecb528e9c46",
+    "url": "/static/js/main.0e6077af.chunk.js"
+  },
+  {
+    "revision": "20718b0050058a9fae17",
+    "url": "/static/js/runtime~main.191dde89.js"
+  }
+]);
